3145d462b0e7774f86c8f3d53d497ba66d765978
movi1 <98534830+movi1@users.noreply.github.com>
2022-09-27 12:40:11 +0100

    add

---
+++ script.js
@@ -0,0 +1,15 @@
+// let x = 4;
+// let y = 10;
+// let z = x + y;
+function calculation() {
+let box1 = document.getElementById("box1").value;
+let box = document.getElementById("box").value;
+let box2 = document.getElementById("box2").value;
+
+
+document.getElementById("calculation").innerHTML = 14;
+
+let sum = (box1 + box2) = 
+
+console.log(calculation)
+}
