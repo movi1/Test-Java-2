3ac3c03c62cbfbefa1361874d481b98450feba44
movi1 <98534830+movi1@users.noreply.github.com>
2022-09-28 12:38:37 +0100

    add javascript

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-// let x = 4;
-// let y = 10;
-// let z = x + y;
-function calculation() {
-let box1 = document.getElementById("box1").value;
-let box = document.getElementById("box").value;
-let box2 = document.getElementById("box2").value;
+// declaring variable so all functions can use them
+let sum;
+let num1 = 0;
+let num2 = 0;
 
 
-document.getElementById("calculation").innerHTML = 14;
+function addBy() {
+    // get element by id in the html
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
 
-let sum = (box1 + box2) = 
+    //reassigning the sum variable with the text 'plus'
+    sum = 'plus';
 
-console.log(calculation)
+    //get the id "symbol" and make its inner text say '+' Example: <p>+</p>
+    document.getElementById("symbol").innerText = '+';
+}
+
+function divideBy() {
+    // get the element by id in html
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+    // reassigning teh sum with the text divide
+    sum = 'divide';
+
+    // get the id "symbol" and make its inner text say'/' Example: <p>/</p>
+    document.getElementById("symbol").innerText = '/';
+}
+// calculate the sum
+function calculate() {
+    if (sum === 'plus') {
+        document.getElementById("result").innerHTML = parseInt(num1) + parseInt(num2);
+    } if (sum === 'divide') {
+        document.getElementById("result").innerHTML = parseInt(num1) / parseInt(num2);
+    } else {
+
+    }
 }
